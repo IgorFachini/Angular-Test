39eedfa7355796b60b9a13736e78861898801774
igorfachini <igor.fachini@catolicasc.org.br>
2017-05-04 12:55:38 -0300

    Finished class 5

--- Angular2x/alurapic2/client/app/foto/foto.module.ts
+++ Angular2x/alurapic2/client/app/foto/foto.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FotoComponent } from './foto.component';
+import { FiltroPorTitulo } from './foto.pipes'
 
 @NgModule({
-  declarations: [ FotoComponent ],
-  exports: [ FotoComponent ]//Permite que outros modulos me utilizem
+  declarations: [ FotoComponent , FiltroPorTitulo],//O que meu modelulo tem, faz parte dele
+  exports: [ FotoComponent, FiltroPorTitulo ]//Permite que outros modulos me utilizem
 })
 export class FotoModule {}
 //tes
