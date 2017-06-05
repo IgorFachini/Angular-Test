d2a55a533713512bf6fb011d2eb0d46cfd0832e9
igorfachini <igor.fachini@catolicasc.org.br>
2017-06-05 14:52:35 -0300

    Finished class 8

--- Angular2x/alurapic2/client/app/foto/foto.component.ts
+++ Angular2x/alurapic2/client/app/foto/foto.component.ts
@@ -3,7 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'foto',
-    templateUrl: './foto.component.html' 
+    templateUrl: './foto.component.html',
+    styleUrls: ['./foto.component.css']
+    //Por padrão, o angular usa ViewEncapsulation com shadow DOM (Emulated),
+    //para que aja compatibilidade em todos os navegadores
 })
 export class FotoComponent {
 
