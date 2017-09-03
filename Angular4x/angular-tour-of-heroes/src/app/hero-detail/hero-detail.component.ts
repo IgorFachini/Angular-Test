23409dcabc229f61d616ba287b009ec219ac4bb8
igorfachini <igor.fachini@catolicasc.org.br>
2017-09-03 02:20:50 -0300

    hero editor routing

--- Angular4x/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Angular4x/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'hero-detail',
@@ -10,9 +13,27 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    ////////////////// DEPRECATED /////////////////////////////
+    // this.route.paramMap
+    //   .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+    //   .subscribe(hero => this.hero = hero);
+    this.route.params.forEach((params) => {
+      if (params['id'] !== undefined) {
+        const id = +params['id'];
+        this.heroService.getHero(id)
+          .then(hero => this.hero = hero);
+      }
+    });
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
